093b6398baa65276945c29189d83352c1e054104
PJ <peterjoe2005@gmail.com>
2025-02-05 12:45:57 -0500

    three blinking lights

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_BUILTIN_PIN 13  // Built-in LED (usually pin 13)
+#define LED_PIN_9  9        // External LED on pin 9
+#define LED_PIN_10 10       // External LED on pin 10
+#define LED_PIN_11 11       // External LED on pin 11
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(LED_BUILTIN_PIN, OUTPUT);
+    pinMode(LED_PIN_9, OUTPUT);
+    pinMode(LED_PIN_10, OUTPUT);
+    pinMode(LED_PIN_11, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    // Turn all LEDs on
+    digitalWrite(LED_BUILTIN_PIN, HIGH);
+    digitalWrite(LED_PIN_9, HIGH);
+    digitalWrite(LED_PIN_10, HIGH);
+    digitalWrite(LED_PIN_11, HIGH);
+    delay(1000); // Wait 1 second
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Turn all LEDs off
+    digitalWrite(LED_BUILTIN_PIN, LOW);
+    digitalWrite(LED_PIN_9, LOW);
+    digitalWrite(LED_PIN_10, LOW);
+    digitalWrite(LED_PIN_11, LOW);
+    delay(1000); // Wait 1 second
 }
