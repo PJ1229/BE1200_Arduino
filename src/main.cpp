08926c6b8fbec64128813e84bb2586d447a06985
PJ <peterjoe2005@gmail.com>
2025-02-17 11:20:35 -0500

    uploaded code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
-#include "snake_game.h"
+#include <Arduino.h>
 
-void setup() {
-    setupGame();
+int   a = 17;
+int   b;
+float c;
+
+void setup() 
+{
+  //initialize serial communication
+  Serial.begin(9600);
+
 }
 
-void loop() {
-    updateGame();
-    renderGame();
+void loop() 
+{
+ 
+  b = a/4;  
+  
+  c = a/4.0;  
+
+Serial.print("b = ");
+Serial.println(b);
+Serial.print("c = ");
+Serial.println(c);
+delay(5000);
+
 }
